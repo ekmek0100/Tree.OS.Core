182fdd7d79a330685c0d26cebab23ff65b6e6f18
ekmek0100 <alikerem99k@gmail.com>
2026-06-28 10:18:46 -0700

    Update treeosui.py

--- Tree OS Core v1.0/systemtree16/Tree_OS_Userland/treeosui.py
+++ Tree OS Core v1.0/systemtree16/Tree_OS_Userland/treeosui.py
@@ -5,11 +5,11 @@ import subprocess
 import pefile
 BootStatus = None
 class Window: #make a window
-def __init__(self, title, width, height)
+def __init__(self, title, width, height):
       self.title = title
       self.width = width
       self.height = height
-	def render(self)
+	def render(self):
    print(f"Drawing window '{self.title}' with size {self.width}x{self.height}")
 def NewWindow(title, width, height)
    return Window(title, width, height)
