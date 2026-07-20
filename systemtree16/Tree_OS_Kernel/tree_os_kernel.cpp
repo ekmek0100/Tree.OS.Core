0e467f0e4a041ef6ea76e31e637a6ab863a8f885
ekmek0100 <alikerem99k@gmail.com>
2026-07-20 10:32:02 +0300

    removing a stupid code from kernel
    Removed password check functionality from the kernel.

--- systemtree16/Tree_OS_Kernel/tree_os_kernel.cpp
+++ systemtree16/Tree_OS_Kernel/tree_os_kernel.cpp
@@ -1,11 +1,1 @@
-#include <iostream>
-int main() {
-int setpasswd = 2014; 
-std::cout << "Enter password: ";
-std::cin >> setpasswd;
-if (setpasswd == 2014) {
-    std::cout << "şifre doğru." << std::endl;
-} else {
-    std::cout << "şifre yanlış." << std::endl;
-return 0;
-}}
+
